57505cdbccfed51c3adb661ec4f80846f2989897
shalinipriya-annadurai <69166925+shalinipriya-annadurai@users.noreply.github.com>
2021-12-09 06:00:46 -0600

    Initial Commit

---
+++ App/TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Ganesh Malaiyappan on 12/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ App/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ganesh Malaiyappan on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
